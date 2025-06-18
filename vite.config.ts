389449913da3ec470d5c7d4c7078e7f04d8489d1
Ravi Jyoti <ravijyoti3@gmail.com>
2025-06-18 21:17:09 +0530

    cofig fix

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,6 @@ import { componentTagger } from "lovable-tagger";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
-  base: "/gau-seva-connect/",
   server: {
     host: "::",
     port: 8080,
